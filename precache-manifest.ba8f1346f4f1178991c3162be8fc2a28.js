e04d65a98f9e2657b4bcab643a8ef5b078b1cbb9
Mauro <mfconsolani@outlook.com>
2020-09-26 15:49:06 +0200

    Updates

---
+++ precache-manifest.ba8f1346f4f1178991c3162be8fc2a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dc60455d2b285f3217ac2dd698a14a",
+    "url": "/React-Tic-Tac-Toe-Deployment/index.html"
+  },
+  {
+    "revision": "3c6e14532263d82e7073",
+    "url": "/React-Tic-Tac-Toe-Deployment/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "e822021ba96456696426",
+    "url": "/React-Tic-Tac-Toe-Deployment/static/js/2.352ea283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tic-Tac-Toe-Deployment/static/js/2.352ea283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6e14532263d82e7073",
+    "url": "/React-Tic-Tac-Toe-Deployment/static/js/main.547b43b3.chunk.js"
+  },
+  {
+    "revision": "ecf25312189bc31fa9b6",
+    "url": "/React-Tic-Tac-Toe-Deployment/static/js/runtime-main.8d2ad94f.js"
+  }
+]);
